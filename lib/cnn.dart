f262b683dc30dd907ad21aeb3bea72cba45e7a06
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2023-12-05 17:35:53 +0100

    fix the tflite issue

--- lib/cnn.dart
+++ lib/cnn.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class cnn extends StatefulWidget {
   const cnn({super.key});
@@ -10,11 +12,46 @@ class cnn extends StatefulWidget {
 
 class _cnnState extends State<cnn> {
 
+final ImagePicker picker=ImagePicker();
+XFile? _imageFile;
 
-   load_image(){
 
+@override
+void initState(){
+  super.initState();
 
-   }
+}
+
+Future initModel() async {
+  String? res = await Tflite.loadModel(
+  model: "assets/model.tflite"
+);
+ return res;
+}
+
+Future<void> _pickImage(ImageSource source) async {
+  XFile? pickedImage = await picker.pickImage(source: source);
+  setState(() {
+    _imageFile = pickedImage;
+
+  });
+}
+
+Future predict() async{
+    var recognitions = await Tflite.runModelOnImage(
+      path: _imageFile!.path,   // required
+      imageMean: 0.0,   // defaults to 117.0
+      imageStd: 255.0,  // defaults to 1.0
+      numResults: 1,    // defaults to 5
+      threshold: 0.2,   // defaults to 0.1
+      asynch: true      // defaults to true
+    );
+
+print(recognitions);
+    return recognitions;
+}
+
+  
   @override
   Widget build(BuildContext context) {
     return 
@@ -39,9 +76,19 @@ class _cnnState extends State<cnn> {
              mainAxisAlignment:MainAxisAlignment.center,
              children: [
     ElevatedButton(onPressed: (){
-      load_image();
+       _pickImage(ImageSource.gallery);
     }, child: const Text('Load an image')),
-    ElevatedButton(onPressed: (){}, child: const Text('Take a picture'))
+    ElevatedButton(onPressed: (){
+       
+            _pickImage(ImageSource.camera);
+    }, 
+    child: const Text('Take a picture')),
+    ElevatedButton(onPressed: (){
+       
+      predict();
+    }, 
+    child: const Text('Predict'))
+
     ],)
         ]
 
